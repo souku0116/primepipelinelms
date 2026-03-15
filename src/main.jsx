c2babd54c81d374df7c9b289677fc31923aab314
souku0116 <85379073+souku0116@users.noreply.github.com>
2026-03-15 08:53:43 +0530

    Import SpeedInsights from Vercel package

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
